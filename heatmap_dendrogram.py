d776e304512c93b2817bd3cb591916c6f155cffb
Ismael Lieberherr <ismi@gmx.ch>
2016-06-21 17:03:04 +0200

    added a star

--- heatmap_dendrogram.py
+++ heatmap_dendrogram.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 '''
-DRAFT
+***DRAFT***
 Runs with Python 3
 Script reading from a csv spread sheet with cognacy judgements (1), computes the hamming distance/similarity matrix (2)
 and creates two plots using the seaborn, pandas and numpy modules (3). Finally a simulation with slightly disturbed data
