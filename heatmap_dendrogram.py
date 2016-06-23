382d9939a21215add5446f6afc3a82110d876da2
Ismael Lieberherr <ismi@gmx.ch>
2016-06-23 14:44:18 +0200

    Slightly improved prose in comments

--- heatmap_dendrogram.py
+++ heatmap_dendrogram.py
@@ -24,7 +24,7 @@ import scipy.spatial.distance as pdist
 ######################################################################
 #(1) Prepare spreadsheet data - convert to nxp matrix (dataframe)
 '''
-The data used for this analysis  comes from a Google spreadsheet with doculects as columns and glosses and our cognacy statement as rows. Roots which we consider as cognate have the same number. Example:
+The data used for this analysis we had entered into a Google spreadsheet having doculects as columns and glosses and our cognacy statement as rows. Roots which we consider as cognate have the same number. Example:
 
 ______________________________________________________ 
 | Gloss\Doculect|Duhumbi |Khispi |Rupa |Shergaon |...
@@ -38,10 +38,10 @@ ______________________________________________________
 Where data is missing the cognacy statement is something which is not an integer 
 e.g. "NA", "n.a.", "" etc.
 
-Ok now we are on the shit branch. The first step is to read the data, extract the cognacy statement and make the missing data uniform, transpose to a nxp multivariate data matrix.
+The first step is to read the data, extract the cognacy statement, transpose to a nxp multivariate data matrix.
 In our case the matrix has the dimensions 29x100, where n = 29 is the number of 
-doculects, and p = 100 is the number of items in the adjusted Leipzig-Jakarta list. 
-Looking something like the following:
+doculects, and p = 100 is the number of glosses in the adjusted Leipzig-Jakarta list. 
+Looking something like the following table:
 
 ___________________________________________
 |Doculect\Gloss |HAND |WATER |...|GLOSS100|
@@ -85,7 +85,7 @@ cognacy_matrix = make_datamatrix_from_spreadsheet(spreadsheet_directory, spreads
 ######################################################################
 #(2) Compute distance/similarity  matrix
 '''
-Every row in cognacy_matrix contains integers (cognacy statement) and "NaN" (missing data). To get the number of shared cognates we use the pre-implememted "Hamming metric" of the scipy module. The Hamming metric counts the distance between two identical integers as 0 and between two not identical integers as 1. If one of the two is missing it is also counted as 0. For example if:
+Every row in cognacy_matrix contains integers (cognacy statement) or "NaN" (missing data). As a measure for the distance between two doculects, we implemented the Hamming metric. The Hamming metric counts the distance between two identical integers as 0 and between two not identical integers as 1. If one of the two is missing it is also counted as 0. For example if:
 
          | gloss1| gloss2| gloss3| gloss4|
 ---------+-------+-------+-------+-------|
@@ -95,15 +95,12 @@ Doculect3|     2 |     2 |      3| 1     |
 
 then:
 
-d(Doculect1, Doculect2) = 1 (only gloss3 is different)
+d(Doculect1, Doculect2) = 1 (only gloss3 is different)
 d(Doculect2, Doculect3) = 2 (gloss1 and gloss3 are different)
 d(Doculect1, Doculect3) = 3 (gloss1, gloss2 and gloss3 are different)
 
-The percentages of different glosses (Hamming distance) would be 1/4, 2/4 and 3/4 and the cognacy percentage between 
-the three fictive doculects would be 75% ((1-0.25)*100), 50% and 25%. However here missing data was counted in the same way 
-as non-cognate forms which is not a sensible approach. It is more appropriate to ommit all pairs with missing data. 
-The distances become 1/2, 3/3 and 2/3, and the cognacy percentages 50%, 0% and 33%. I.e. the percentage is computed 
-on basis of the available words, and not on basis of the whole data, which is more realistic.
+The percentages of different glosses (Hamming distance) would be 1/4, 2/4 and 3/4 and the cognacy percentage (or similarity) between the three fictive doculects would be 75% ((1-0.25)*100), 50% and 25%. However here missing data was counted in the same way 
+as non-cognate forms which is not a sensible approach (but which is done by the pre-implemented Hamming metric of pandas). It is more appropriate to ommit all pairs with missing data. The distances become 1/2, 3/3 and 2/3, and the cognacy percentages 50%, 0% and 33%. I.e. the percentage is computed on basis of the available words, and not on basis of the whole data.
 '''
 ######################################################################
 
@@ -144,14 +141,13 @@ pairwise_cognacy = hamming_similarity(cognacy_matrix)*100
 ######################################################################
 #(3) Perform cluster analysis and plot data
 '''
-Hierarchical clusteranalysis with unrounded similarity  matrix.
+Hierarchical clusteranalysis with unrounded similarity  matrix. The preimplemented scipy algorithm was used, which is explained in the paper.
 '''
 ######################################################################
 
 def plot_heatmap_with_dendrogram(similarity_matrix, plot_name):
     '''
-    Makes a plot of heatmap and dendrogram. Arguments are a similarity matrix (as Pandas data frame) 
-    and the name of the plot. 
+    Makes a plot of heatmap and dendrogram. Arguments are a similarity matrix (as Pandas data frame) and the name of the plot. 
     '''
     #the distance matrix has to be condensed first
     cond_dist_matrix = pdist.squareform(100 - similarity_matrix)
@@ -168,44 +164,49 @@ def plot_heatmap_with_dendrogram(similarity_matrix, plot_name):
     plt.setp(heatncluster.ax_heatmap.xaxis.get_majorticklabels(), rotation=45)
     plt.savefig(plot_name + ".png")
 
-
+#make two plots: one for the Kho-Bwa languages only, and one for all languages
 plot_heatmap_with_dendrogram(pairwise_cognacy, 'tbkhobwa')
 plot_heatmap_with_dendrogram(pairwise_cognacy.iloc[:21,:21],'khobwa')
 
 ######################################################################
 #(4) Simulation
 '''
+Motivation for the simulation was the question whether and how much different data would change the dendrogram that we obtained. Would slightly different data and cognacy decisions change the overal result? For added a matrix with random values to our matrix, made a plot like in (3) and checked "manually" whether the result was different. 
 
 '''
 
-def create_random_matrix(distr, mean, variation, dim):
+def create_random_matrix(distr, mean, spread, dim):
     """
     Function to create a matrix with random entries.
     distr: probability distribution (string)
-    mean: more strict or less strict in average 
-    variation: how much variation (not variance)
+    mean: are the random values centered around the actual value (mean = 0)? Or are all values generally lower/higher? (simulating more strict/more speculative cognacy decisions)
+    spread: if value is 10 then the maximum value that might be added is +-10
     dim: size of the matrix list [,]
     """
     if distr == 'binomial':
-        random_matrix = np.random.binomial(n=variation*2, p=0.5, size=dim)-variation + mean
+        random_matrix = np.random.binomial(n=spread*2, p=0.5, size=dim)-spread + mean
         #make a symmetric random matrix (transpose, add divide by two)
         random_matrix = (random_matrix + random_matrix.T)/2
     elif distr == 'uniform':
-        random_matrix = np.random.randint(-variation, high=variation, size=dim) + mean
+        random_matrix = np.random.randint(-spread, high=spread, size=dim) + mean
         random_matrix = (random_matrix + random_matrix.T)/2
     return random_matrix   
     
 
 #Kho-Bwa with random variation (looks obiously different every time produced)
-def simulate(data_set, no_sim, distr, variation, mean, out_directory, pltname):
+def simulate(data_set, no_sim, distr, spread, mean, out_directory, pltname):
     """
+    Function to run the simulation.
     data_set: nxn data set with cognacy percentages
     no_sim: number of simulations
+    distr: probability distribution
+    spread: if value is 10 then the maximum value that might be added is +-10
     out_directory: where to save the plots
+    pltname: plots have the name of the value of the sting in pltname, plus an integer for every simulation. E.g. simulation_1.png, simulation_2.png,...
     """
     dim = data_set.shape
     for i in range(no_sim):
-        random_matrix = create_random_matrix(distr, mean, variation, dim)
+        random_matrix = create_random_matrix(distr, mean, spread, dim)
         data_plus_random_matrix = data_set + random_matrix
         #cognacy percentage can not be more than 100
         data_plus_random_matrix[data_plus_random_matrix>100] = 99
